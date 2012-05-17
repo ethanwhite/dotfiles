f9d9f6f78707e271710a24c160a2d177c0b0adeb
Ethan White <ethan@weecology.org>
2012-05-17 15:14:37 -0600

    ADD SCRIPT FOR AUTOMATICALLY SIM LINKING FILES TO HOME FOLDER

---
+++ link_files.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Easily link all the dotfiles after a `git pull`.
+#
+# Usage:
+# bash link_files.sh
+#
+# By Zach Will (https://github.com/zachwill/dotfiles/blob/master/link_files.sh)
+
+# Colors...
+GREEN='\033[32m'
+RED='\033[31m'
+BLUE='\033[34m'
+NC='\033[0m' # No Color
+
+
+link_file() {
+  ln -s $PWD/$file $HOME/$file && echo "${GREEN}Linked ~/$file ${NC}"
+}
+
+rm_and_link() {
+  rm $HOME/$file && link_file
+}
+
+prompt_deletion() {
+  read -n1 -p "Delete ~/$file and link new one? (y/n) "
+  printf "\n"
+  [[ $REPLY == [yY] ]] && rm_and_link || echo "${RED}File not linked.${NC}"
+}
+
+
+# Loop through available dotfiles...
+for file in $(ls -a | grep ^\\.[a-z] | grep -v .git$); do
+if [ -e $HOME/$file ]; then
+prompt_deletion
+  else
+link_file
+  fi
+done
+
+# All done.
+echo "\n${BLUE}Returning to shell...${NC}\n"
